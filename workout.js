08f1e8d3515aa823cee361de4b4aea172ea04d62
PiiuKa <157690096+PiiuKa@users.noreply.github.com>
2026-04-21 14:45:46 +0300

    Add basic workouts module
    Introduce a new workouts module

---
+++ workout.js
@@ -0,0 +1,21 @@
+const workouts = [];
+
+const createWorkout = (username) => {
+  if (typeof username !== "string" || username.length < 4) {
+    throw new Error("the parameter username is not valid");
+  }
+  const workout = { id: workouts.length + 1, username, exercises: [] };
+  workouts.push(workout);
+  return workout;
+};
+
+const addExercise = (workoutId, exerciseName) => {
+  const workout = workouts.find((w) => w.id === workoutId);
+  if (!workout) {
+    throw new Error(`Workout ${workoutId} not found`);
+  }
+  workout.exercises.push(exerciseName);
+  return { workoutId, exerciseName };
+};
+
+export { addExercise, createWorkout, workouts };
